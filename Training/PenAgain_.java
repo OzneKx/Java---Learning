c693c5df0078361311752c7cfe7e7c575e8e1c6e
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-12 18:37:05 -0300

    PenAgain_ - JAVA

---
+++ Training/PenAgain_.java
@@ -0,0 +1,11 @@
+package Training;
+
+public class PenAgain_ {
+  public static void main(String[] args) {
+    // Creating an object
+    PenAgain pen1 = new PenAgain();
+    pen1.setModel("BIC");
+    pen1.setTip(0.7f);
+    pen1.status();
+  }
+}
