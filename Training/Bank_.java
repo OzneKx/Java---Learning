1c583cdf882ee9e529b7e31c75f3105b27b1e86a
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-15 11:37:34 -0300

    Bank_ - JAVA

---
+++ Training/Bank_.java
@@ -0,0 +1,17 @@
+package Training;
+
+public class Bank_ {
+  public static void main(String[] args) {
+    // Creating client1 who is Jubileu
+    Bank client1 = new Bank(123, "cp", "Jubileu", 0, false);
+
+    // Using Methods
+    client1.
+
+
+    // Creating client2 who is Creusa
+    Bank client2 = new Bank(321, "cc", "Creusa", 0,  false);
+
+    // Using Methods
+  }
+}
