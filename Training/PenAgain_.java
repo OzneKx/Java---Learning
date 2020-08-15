66e9f320255563db426faa50bd639f1e8ee7ade5
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-15 09:40:10 -0300

    Pen Again _ - JAVA

--- Training/PenAgain_.java
+++ Training/PenAgain_.java
@@ -3,9 +3,9 @@ package Training;
 public class PenAgain_ {
   public static void main(String[] args) {
     // Creating an object
-    PenAgain pen1 = new PenAgain();
-    pen1.setModel("BIC");
-    pen1.setTip(0.7f);
+    PenAgain pen1 = new PenAgain("NIC", "Amarelo", 0.5f);
     pen1.status();
+    PenAgain pen2 = new PenAgain("Faber Castel", "Azul", 0.7f);
+    pen2.status();
   }
 }
