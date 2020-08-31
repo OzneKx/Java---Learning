b1d2a5245c6f04cff9ffb9dcc78478000faf9ec4
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-08-31 08:03:25 -0300

    Control Continuation - JAVA

--- Training/ControlContinuation.java
+++ Training/ControlContinuation.java
@@ -1,13 +1,12 @@
 package Training;
 
 public class ControlContinuation {
-  public static void main(string[] args) {
+  public static void main(String[] args) {
     RemoteControl control = new RemoteControl();
     control.turnOn();
     control.increaseVolume();
     control.play();
     control.openMenu();
     control.closeMenu();
-
   }
 }
