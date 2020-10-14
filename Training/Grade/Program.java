eb0a0782ec759349da194995ea0ab844263ae26f
Kenzo De Albuquerque <kenzoalbuqk@gmail.com>
2020-10-14 15:38:51 -0300

    Learning - JAVA

--- Training/Grade/Program.java
+++ Training/Grade/Program.java
@@ -9,11 +9,15 @@ public class Program {
 
     System.out.print("Nome do aluno: ");
     student.name = sc.nextLine();
-    System.out.print("Nota - 1° Trimestre:");
+    System.out.print("Nota - 1° Trimestre: ");
     student.score1 = sc.nextDouble();
-    System.out.print("Nota - 2° Trimestre:");
+    System.out.print("Nota - 2° Trimestre: ");
     student.score2 = sc.nextDouble();
-    System.out.print("Nota - 3° Trimestre:");
+    System.out.print("Nota - 3° Trimestre: ");
     student.score3 = sc.nextDouble();
+
+    System.out.println(student);
+
+    sc.close();
   }
 }
--- Training/Grade/Student.java
+++ Training/Grade/Student.java
@@ -6,4 +6,29 @@ public class Student {
   public double score1;
   public double score2;
   public double score3;
+  public String status;
+
+  // Methods
+  public double anualScore() {
+    return (score1 + score2 + score3); 
+  }
+
+  public double calculus() {
+    double quantity = (100 * 0.6); 
+    return (quantity - anualScore());
+  } 
+
+  public String condition() {
+    if (anualScore() >= 60) {
+      this.status = "PASS";
+    } else {
+      this.status = "FAILED";
+    }  System.out.println("\nMISSING " + calculus() + " POINTS");
+    return status;
+  }
+
+  @Override
+  public String toString() {
+    return "FINAL GRADE = " + String.format("%.2f", anualScore()) + "\n" + condition();
+  }
 }
